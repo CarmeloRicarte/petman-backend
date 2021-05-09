e40784b07aa62fdfd84ffb9ce0f71010989600de
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2021-05-09 12:53:18 +0200

    cambio en rutas

--- helpers/menu-frontend.js
+++ helpers/menu-frontend.js
@@ -4,39 +4,39 @@ const getMenuFrontend = (rol = 'USER_ROLE') => {
             titulo: 'Principal',
             icono: 'pi pi-home',
             submenu: [
-                { titulo: 'Dashboard', url: '/dashboard', icono: 'pi pi-info-circle' },
-                { titulo: 'Gráficos', url: '/graficos', icono: 'pi pi-chart-bar', }
+                { titulo: 'Dashboard', url: 'dashboard', icono: 'pi pi-info-circle' },
+                { titulo: 'Gráficos', url: 'graficos', icono: 'pi pi-chart-bar', }
             ]
         },
         {
             titulo: 'Gestión de productos',
             icono: 'pi pi-folder',
             submenu: [
-                { titulo: 'Productos', url: '/productos', icono: 'pi pi-folder-open' },
-                { titulo: 'Categorías', url: '/categorias', icono: 'pi pi-caret-up' },
-                { titulo: 'Subcategorias', url: '/subcategorias', icono: 'pi pi-caret-down' },
+                { titulo: 'Productos', url: 'productos', icono: 'pi pi-folder-open' },
+                { titulo: 'Categorías', url: 'categorias', icono: 'pi pi-caret-up' },
+                { titulo: 'Subcategorias', url: 'subcategorias', icono: 'pi pi-caret-down' },
             ]
         },
         {
             titulo: 'Gestión de logística',
             icono: 'pi pi-list',
             submenu: [
-                { titulo: 'Envíos de mercancía', url: '/envio-mercancia', icono: 'pi pi-send', },
-                { titulo: 'Recepciones de mercancía', url: '/recepcion-mercancia', icono: 'pi pi-directions-alt', },
+                { titulo: 'Envíos de mercancía', url: 'envio-mercancia', icono: 'pi pi-send', },
+                { titulo: 'Recepciones de mercancía', url: 'recepcion-mercancia', icono: 'pi pi-directions-alt', },
             ]
         },
         {
             titulo: 'Clientes y Proveedores',
             icono: 'pi pi-phone',
             submenu: [
-                { titulo: 'Listado de Clientes', url: '/clientes', icono: 'pi pi-id-card' },
-                { titulo: 'Listado de Proveedores', url: '/proveedores', icono: 'pi pi-briefcase' },
+                { titulo: 'Listado de Clientes', url: 'clientes', icono: 'pi pi-id-card' },
+                { titulo: 'Listado de Proveedores', url: 'proveedores', icono: 'pi pi-briefcase' },
             ]
         }
     ];
 
     if (rol === 'ADMIN_ROLE') {
-        menu[0].submenu.unshift({ titulo: 'Usuarios', url: '/usuarios', icono: 'pi pi-users' });
+        menu[0].submenu.unshift({ titulo: 'Usuarios', url: 'usuarios', icono: 'pi pi-users' });
     }
 
     return menu;
