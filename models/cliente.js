cc7b8b7591cd6817a9fb04faa0def412b23dcf76
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2021-05-30 09:11:22 +0200

    Unificado nombre y apellidos en nombre

--- models/cliente.js
+++ models/cliente.js
@@ -1,44 +1,35 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const ClienteSchema = Schema({
-
-    nombre: {
-        type: String,
-        required: true
-    },
-    apellidos: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    poblacion: {
-        type: String,
-        required: true,
-    },
-    direccion: {
-        type: String,
-        required: true,
-    },
-    telefono: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    email: {
-        type: String
-    },
-    dni: {
-        type: String
-    }
+  nombre: {
+    type: String,
+    required: true,
+  },
+  poblacion: {
+    type: String,
+    required: true,
+  },
+  direccion: {
+    type: String,
+    required: true,
+  },
+  telefono: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+  },
+  dni: {
+    type: String,
+  },
 });
 
+ClienteSchema.method("toJSON", function () {
+  const { __v, _id, ...object } = this.toObject();
+  object.uid = _id;
+  return object;
+});
 
-ClienteSchema.method('toJSON', function () {
-    const { __v, _id, ...object } = this.toObject();
-    object.uid = _id;
-    return object;
-})
-
-
-
-module.exports = model('Cliente', ClienteSchema);
+module.exports = model("Cliente", ClienteSchema);
--- routes/backoffice/clientes.js
+++ routes/backoffice/clientes.js
@@ -1,57 +1,53 @@
 /*
     Ruta: /api/clientes
 */
-const { Router } = require('express');
-const { check } = require('express-validator');
-const { validarCampos } = require('../../middlewares/validar-campos');
+const { Router } = require("express");
+const { check } = require("express-validator");
+const { validarCampos } = require("../../middlewares/validar-campos");
 
-const { getClientes, getClienteById, crearCliente, actualizarCliente, borrarCliente, borrarClientesSeleccionados } = require('../../controllers/backoffice/clientes');
-const { validarJWT } = require('../../middlewares/validar-jwt');
-
+const {
+  getClientes,
+  getClienteById,
+  crearCliente,
+  actualizarCliente,
+  borrarCliente,
+  borrarClientesSeleccionados,
+} = require("../../controllers/backoffice/clientes");
+const { validarJWT } = require("../../middlewares/validar-jwt");
 
 const router = Router();
 
+router.get("/", validarJWT, getClientes);
 
-router.get('/', validarJWT, getClientes);
+router.get("/:uid", validarJWT, getClienteById);
 
-router.get('/:uid',
+router.post(
+  "/",
+  [
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("poblacion", "La población es obligatoria").not().isEmpty(),
+    check("direccion", "La dirección es obligatoria").not().isEmpty(),
+    check("telefono", "El teléfono es obligatorio").notEmpty(),
+    validarCampos,
+  ],
+  crearCliente
+);
+
+router.put(
+  "/:uid",
+  [
     validarJWT,
-    getClienteById
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("poblacion", "La población es obligatoria").not().isEmpty(),
+    check("direccion", "La dirección es obligatoria").not().isEmpty(),
+    check("telefono", "El teléfono es obligatorio").notEmpty(),
+    validarCampos,
+  ],
+  actualizarCliente
 );
 
-router.post('/',
-    [
-        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
-        check('apellidos', 'Los apellidos son obligatorios').not().isEmpty(),
-        check('poblacion', 'La población es obligatoria').not().isEmpty(),
-        check('direccion', 'La dirección es obligatoria').not().isEmpty(),
-        check('telefono', 'El teléfono es obligatorio').notEmpty(),
-        validarCampos,
-    ],
-    crearCliente
-);
+router.delete("/:uid", validarJWT, borrarCliente);
 
-router.put('/:uid',
-    [
-        validarJWT,
-        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
-        check('apellidos', 'Los apellidos son obligatorios').not().isEmpty(),
-        check('poblacion', 'La población es obligatoria').not().isEmpty(),
-        check('direccion', 'La dirección es obligatoria').not().isEmpty(),
-        check('telefono', 'El teléfono es obligatorio').notEmpty(),
-        validarCampos,
-    ],
-    actualizarCliente
-);
-
-router.delete('/:uid',
-    validarJWT,
-    borrarCliente,
-);
-
-router.post('/deleteSelected',
-    validarJWT,
-    borrarClientesSeleccionados
-)
+router.post("/deleteSelected", validarJWT, borrarClientesSeleccionados);
 
 module.exports = router;
