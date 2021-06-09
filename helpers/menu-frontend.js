955def078b9bd76130bd0417d285fff73f102747
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2021-06-09 20:13:53 +0200

    actualizado menú para tpv

--- helpers/menu-frontend.js
+++ helpers/menu-frontend.js
@@ -1,47 +1,83 @@
-const getMenuFrontend = (rol = 'USER_ROLE') => {
-    const menu = [
+const getMenuFrontend = (rol = "USER_ROLE") => {
+  const menu = [
+    {
+      titulo: "Principal",
+      icono: "pi pi-home",
+      submenu: [
+        { titulo: "Dashboard", url: "dashboard", icono: "pi pi-info-circle" },
         {
-            titulo: 'Principal',
-            icono: 'pi pi-home',
-            submenu: [
-                { titulo: 'Dashboard', url: 'dashboard', icono: 'pi pi-info-circle' },
-                { titulo: 'Gráficos', url: 'graficos', icono: 'pi pi-chart-bar', }
-            ]
+          titulo: "Ventas",
+          icono: "pi pi-shopping-cart",
+          submenu: [
+            { titulo: "Crear Venta", url: "tpv/crear", icono: "pi pi-ticket" },
+            {
+              titulo: "Listado de Ventas",
+              url: "tpv/listado-ventas",
+              icono: "pi pi-list",
+            },
+          ],
+        },
+        { titulo: "Gráficos", url: "graficos", icono: "pi pi-chart-bar" },
+      ],
+    },
+    {
+      titulo: "Gestión de productos",
+      icono: "pi pi-folder",
+      submenu: [
+        { titulo: "Productos", url: "productos", icono: "pi pi-folder-open" },
+        { titulo: "Categorías", url: "categorias", icono: "pi pi-caret-up" },
+        {
+          titulo: "Subcategorias",
+          url: "subcategorias",
+          icono: "pi pi-caret-down",
+        },
+      ],
+    },
+    {
+      titulo: "Gestión de logística",
+      icono: "pi pi-list",
+      submenu: [
+        {
+          titulo: "Envíos de mercancía",
+          url: "envio-mercancia",
+          icono: "pi pi-send",
         },
         {
-            titulo: 'Gestión de productos',
-            icono: 'pi pi-folder',
-            submenu: [
-                { titulo: 'Productos', url: 'productos', icono: 'pi pi-folder-open' },
-                { titulo: 'Categorías', url: 'categorias', icono: 'pi pi-caret-up' },
-                { titulo: 'Subcategorias', url: 'subcategorias', icono: 'pi pi-caret-down' },
-            ]
+          titulo: "Recepciones de mercancía",
+          url: "recepcion-mercancia",
+          icono: "pi pi-directions-alt",
+        },
+      ],
+    },
+    {
+      titulo: "Clientes y Proveedores",
+      icono: "pi pi-phone",
+      submenu: [
+        {
+          titulo: "Listado de Clientes",
+          url: "clientes",
+          icono: "pi pi-id-card",
         },
         {
-            titulo: 'Gestión de logística',
-            icono: 'pi pi-list',
-            submenu: [
-                { titulo: 'Envíos de mercancía', url: 'envio-mercancia', icono: 'pi pi-send', },
-                { titulo: 'Recepciones de mercancía', url: 'recepcion-mercancia', icono: 'pi pi-directions-alt', },
-            ]
+          titulo: "Listado de Proveedores",
+          url: "proveedores",
+          icono: "pi pi-briefcase",
         },
-        {
-            titulo: 'Clientes y Proveedores',
-            icono: 'pi pi-phone',
-            submenu: [
-                { titulo: 'Listado de Clientes', url: 'clientes', icono: 'pi pi-id-card' },
-                { titulo: 'Listado de Proveedores', url: 'proveedores', icono: 'pi pi-briefcase' },
-            ]
-        }
-    ];
+      ],
+    },
+  ];
 
-    if (rol === 'ADMIN_ROLE') {
-        menu[0].submenu.unshift({ titulo: 'Usuarios', url: 'usuarios', icono: 'pi pi-users' });
-    }
+  if (rol === "ADMIN_ROLE") {
+    menu[0].submenu.unshift({
+      titulo: "Usuarios",
+      url: "usuarios",
+      icono: "pi pi-users",
+    });
+  }
 
-    return menu;
+  return menu;
 };
 
 module.exports = {
-    getMenuFrontend
-}
+  getMenuFrontend,
+};
